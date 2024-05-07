60b2c24ad645a2ef8728634e248a6c5685e30f4a
Deann7 <deandronas04@gmail.com>
2024-05-07 22:50:53 +0700

    fix page

---
+++ app/routes/loginBerhasil.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loginBerhasil() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-[#FFC300] mb-6">LOGIN BERHASIL</h1>
+    </div>
+  )
+}
